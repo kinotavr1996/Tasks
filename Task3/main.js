13009bb83ace3fdc09fc58367a4521afff2916c1
kinotavr1996 <olegolegovich1996@gmail.com>
2017-03-21 19:20:47 +0200

    [+] fix some bugs

--- Task3/main.js
+++ Task3/main.js
@@ -1,31 +1,76 @@
-$(function () {
-    var alreadyFilled = false;
-    var states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+$(document).ready(function(){
+	
+	var states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+	var newArray = [];
+	for(var i = 0; i < states.length - 1;i++){
+	newArray.push({id:i,name:states[i]});
+	
+}
+	var options = {
+		source: newArray,
+		key: "id",
+		label: "name",
+		changed: function(s){alert('You choose ' + s.name)}
+	};
+	jQuery('input.autocomplete').autocomplete(options);
+	
+});
 
+
+
+(function ($) {
+	$.fn.autocomplete = function(options){
+		
+    var alreadyFilled = false,
+		selector = this,
+		dialog = '<div class="dialog"></div>',
+		_close = '<span class="autocomplete_close">Cancel</span>',
+		states = options.source;
+	$(this).after(_close);
+	$(this).after(dialog);
+	
+	
     function initDialog() {
         clearDialog();
-        for (var i = 0; i < states.length; i++) {
-            $('.dialog').append('<div>' + states[i] + '</div>');
+		console.log( options["key"]);
+        for (var i = 0; i < states.length; i++) {		
+			
+            $('.dialog').append('<div data-id="' + states[i][options['key']] + '">' + options.source[i][options['label']] + '</div>');
         }
     }
     function clearDialog() {
         $('.dialog').empty();
     }
-    $('.autocomplete input').click(function () {
+    $(this).click(function () {
         if (!alreadyFilled) {
             $('.dialog').addClass('open');
         }
 
     });
     $('body').on('click', '.dialog > div', function () {
-        $('.autocomplete input').val($(this).text()).focus();
-        $('.autocomplete .close').addClass('visible');
+		/*
+		$(selector).val($(this).attr('data-id'));
+        $(selector).text($(this).text()).focus();
+		*/
+		for(var i= 0;i < states.length;i++){
+			if(states[i][options['key']] == $(this).attr('data-id')){
+				options.changed(states[i]);
+			}
+		}
+		
+		
+		
+        $(selector).val($(this).text()).focus();
+		$(selector).attr('data-id',$(this).attr('data-id'));
+        $('.autocomplete_close').addClass('visible');
         alreadyFilled = true;
     });
-    $('.autocomplete .close').click(function () {
+    $('.autocomplete_close').click(function () {
+		clearDialog();
+		initDialog();
         alreadyFilled = false;
         $('.dialog').addClass('open');
-        $('.autocomplete input').val('').focus();
+        $(this).val('').focus();
         $(this).removeClass('visible');
     });
 
@@ -33,24 +78,27 @@ $(function () {
         str = str.toLowerCase();
         clearDialog();
         for (var i = 0; i < states.length; i++) {
-            if (states[i].toLowerCase().indexOf(str) !== -1) {
-                temp = states[i].split(str);
-				if(!temp[1]) temp[1] = '';
-                $('.dialog').append('<div>' + temp[0] + '<b>' + str + '</b>'+ temp[1] + '</div>');
+            if (options.source[i][options['label']].toLowerCase().indexOf(str) !== -1) {
+                temp = states[i][options['label']].split(str);
+				if(!temp[1])
+					$('.dialog').append('<div>' + temp[0] +'</div>');
+				if(temp[1])
+					$('.dialog').append('<div>' + temp[0] + '<b>' + str + '</b>'+ temp[1] + '</div>');
                 temp = null;
             }
         }   
     }
-    $('.autocomplete input').on('input', function () {
+    $(this).on('input', function () {
         $('.dialog').addClass('open');
         alreadyFilled = false;
          if($(this).val())
              match($(this).val());
     });
     $('body').click(function (e) {
-        if (!$(e.target).is("input, .close")) {
+        if (!$(e.target).is("input, .autocomplete_close")) {
             $('.dialog').removeClass('open');
         }
     });
     initDialog();
-});
+	}
+})(jQuery);
