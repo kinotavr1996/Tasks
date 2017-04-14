6aee431119e4d46528d4b3386b8613b9d74267b3
Yurij Sopila <yurij.sopila@gmail.com>
2017-04-14 16:46:06 +0300

    refactor

--- Task2/gulpfile.js
+++ Task2/gulpfile.js
@@ -1,14 +1,26 @@
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('sass', function () {
- gulp.src('./sass/**/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}))
-   .pipe(gulp.dest('./css'));
+gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./stylesheets'));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('clean',function(){
+    return del('css');
+});
+
+gulp.task('build', ['clean','sass','sass:watch']);
+
+gulp.task('default', ['build']);
