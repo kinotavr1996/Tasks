84e45287ea523eb1f9c581e95f079aba85df2d14
Yurij Sopila <yurij.sopila@gmail.com>
2017-04-14 17:22:02 +0300

    gulp modify

--- Task2/gulpfile.js
+++ Task2/gulpfile.js
@@ -4,23 +4,36 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-uglify');
+
 
 gulp.task('sass', function () {
 gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./stylesheets'));
+    .pipe(gulp.dest('./stylesheets'))
 });
- 
+
+gulp.task('js', function() {
+  gulp.src('./scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(minify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('minjs'))
+});
+
+
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('clean',function(){
-    return del('css');
+    del('stylesheets');
+    del('minjs');
+    return true;
 });
 
-gulp.task('build', ['clean','sass','sass:watch']);
+gulp.task('build', ['clean','sass','js','sass:watch']);
 
 gulp.task('default', ['build']);
