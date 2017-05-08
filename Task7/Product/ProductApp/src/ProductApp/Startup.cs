6c6122c847be520a6c605ce8b1c81015bdb84f73
Yurij Sopila <yurij.sopila@gmail.com>
2017-05-08 13:38:21 +0300

    +

--- Task7/Product/ProductApp/src/ProductApp/Startup.cs
+++ Task7/Product/ProductApp/src/ProductApp/Startup.cs
@@ -72,7 +72,7 @@ namespace Crudtest
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
             DbInitializer.Initialize(context);
-            context.Database.MigrateAsync();
+           
         }
     }
 }
