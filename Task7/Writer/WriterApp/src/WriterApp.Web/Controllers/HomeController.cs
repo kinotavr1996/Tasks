aba122e481170b646e8a9019bbcad60899473d4c
Yurij Sopila <yurij.sopila@gmail.com>
2017-05-10 16:41:31 +0300

    [master] refactoring

--- Task7/Writer/WriterApp/src/WriterApp.Web/Controllers/HomeController.cs
+++ Task7/Writer/WriterApp/src/WriterApp.Web/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WriterApp.Web.Controllers
             else
                 return query;
         }
-        public IQueryable<Writer> ApplySortOrder(IQueryable<Writer> query, string sortOrder,string direction)
+        public IQueryable<Writer> ApplySortOrder(IQueryable<Writer> query, string sortOrder, string direction)
         {
             switch (sortOrder)
             {
@@ -40,25 +40,25 @@ namespace WriterApp.Web.Controllers
             }
             return query;
         }
-        public async Task<IActionResult> Index(string sortOrder, string direction , string searchString, string currentFilter, int? page)
+        public async Task<IActionResult> Index(string sortOrder, string direction, string searchString, string currentFilter, int? page)
         {
             WriterListViewModel writerList = new WriterListViewModel();
             writerList.Filter = searchString;
             writerList.Order.Column = sortOrder;
             writerList.Order.Direction = direction ?? "ASC";
             writerList.Page = page ?? 1;
-            if (writerList.Filter != null)            
-                writerList.Page = 1;            
-            else           
-                writerList.Filter = currentFilter;             
-            var writers = _writerRepository.GetPage(writerList.Page, writerList.PageSize,(query) => ApplySortOrder(ApplyFilter(query,searchString), writerList.Order.Column, writerList.Order.Direction)); 
+            if (writerList.Filter != null)
+                writerList.Page = 1;
+            else
+                writerList.Filter = currentFilter;
+            var writers = _writerRepository.GetPage(writerList.Page, writerList.PageSize, (query) => ApplySortOrder(ApplyFilter(query, searchString), writerList.Order.Column, writerList.Order.Direction));
             foreach (var c in writers)
             {
                 writerList.Items.Add(new WriterGridModel { Id = c.Id, FullName = $"{c.LastName} {c.FirstName}", DateOfBirth = c.DateOfBirth, Biography = c.Biography });
             }
             writerList.TotalPages = (int)Math.Ceiling(writers.TotalCount / (double)writerList.PageSize);
-            return View(writerList);    
-        }  
+            return View(writerList);
+        }
 
         [HttpGet]
         public async Task<IActionResult> Create()
@@ -71,14 +71,15 @@ namespace WriterApp.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                _writerRepository.Add(new Writer {
+                _writerRepository.Add(new Writer
+                {
                     FirstName = writer.FirstName,
                     LastName = writer.LastName,
                     DateOfBirth = writer.DateOfBirth,
                     Biography = writer.Biography
                 });
                 return RedirectToAction("Index");
-            } 
+            }
             return View(writer);
         }
 
@@ -86,7 +87,8 @@ namespace WriterApp.Web.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             var writer = _writerRepository.GetById(id);
-            return View(new WriterEditModel {
+            return View(new WriterEditModel
+            {
                 FirstName = writer.FirstName,
                 LastName = writer.LastName,
                 DateOfBirth = writer.DateOfBirth,
@@ -97,10 +99,11 @@ namespace WriterApp.Web.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(WriterEditModel writer)
-        {  
+        {
             if (ModelState.IsValid)
             {
-                _writerRepository.Edit(new Writer {
+                _writerRepository.Edit(new Writer
+                {
                     Id = writer.Id,
                     FirstName = writer.FirstName,
                     LastName = writer.LastName,
