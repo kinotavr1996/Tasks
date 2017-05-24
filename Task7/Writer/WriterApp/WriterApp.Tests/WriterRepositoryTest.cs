cf4b41b42af76a660424e7e3e0b2e42958e14d8b
Yurij Sopila <yurij.sopila@gmail.com>
2017-05-24 20:16:04 +0300

    [master] add some project files

---
+++ Task7/Writer/WriterApp/WriterApp.Tests/WriterRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WriterApp.Tests
+{
+    public class WriterRepositoryTest
+    {
+        public WriterRepositoryTest()
+        {
+        }
+    }
+}
